90994b431c2be16489fbe5a6726c641b7998e7ba
unknown <samuelpano@gmail.com>
2022-10-31 15:52:14 +0800

    added styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,6 @@
 import * as React from "react";
 import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
+import { AppBar, Box, Toolbar, Typography, InputBase } from "@mui/material/";
 import SearchIcon from "@mui/icons-material/Search";
 
 const Search = styled("div")(({ theme }) => ({
@@ -48,7 +44,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
   },
 }));
-
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
